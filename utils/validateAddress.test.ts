2d46bc6c038bd28c7c6a0c2078341e985768ea0a
Loong Yong En <yongen.loong@aelf.io>
2023-10-27 17:29:05 +0800

    feat: add test cases

---
+++ utils/validateAddress.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, test } from "vitest";
+import { validateAddress } from "./validateAddress";
+
+describe("test validateAddress", () => {
+  test.each([
+    "elf",
+    "ELF",
+    "ELF_",
+    "_tdvw",
+    "_tDVW",
+    "tdvw",
+    "tDVW",
+    "cDPLA9axUVeujnTTk4Cyr3aqRby3cHHAB6Rh28o7BRTTxi8US_AELF",
+    "cDPLA9axUVeujnTTk4Cyr3aqRby3cHHAB6Rh28o7BRTTxi8US_",
+    "_cDPLA9axUVeujnTTk4Cyr3aqRby3cHHAB6Rh28o7BRTTxi8US",
+    "ELF_cDPLA9axUVeujnTTk4Cyr3aqRby3cHHAB6Rh28o7BRTTxi8US",
+  ])("should throw: %s", (a) => {
+    expect(() => validateAddress(a)).toThrowError();
+  });
+
+  test.each([
+    "",
+    "cDPLA9axUVeujnTTk4Cyr3aqRby3cHHAB6Rh28o7BRTTxi8US",
+    "ELF_cDPLA9axUVeujnTTk4Cyr3aqRby3cHHAB6Rh28o7BRTTxi8US_AELF",
+  ])("should not throw: %s", (a) => {
+    expect(() => validateAddress(a)).not.toThrowError();
+  });
+});
